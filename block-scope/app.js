7c8f328f3710ee6ae674406f45e751c4359e3bf5
Kristopher Williams <kristopherwilliams@Kristophers-Mac-mini.local>
2021-12-08 22:05:13 -0600

    block scope

---
+++ block-scope/app.js
@@ -0,0 +1,20 @@
+// let name = 'Kris';
+// let hobbies
+
+// if(name === 'Kris') {
+//   hobbies = ['Sports', 'Cooking'];
+//   console.log(hobbies);
+// }
+
+// function greet () {
+//   let age = 37;
+//   console.log(name, age)
+// }
+
+// console.log(name, hobbies)
+
+// greet();
+
+console.log(userName)
+
+let userName = 'Kris'
